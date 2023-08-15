3ab23ed6a212d1832b1bca6b9f4b0902b0a5e594
Adam <adam.j.mccor@gmail.com>
2023-08-15 13:19:50 +0100

    one, added main function call

--- fetchPokemonEvolution.js
+++ fetchPokemonEvolution.js
@@ -50,6 +50,10 @@ async function main() {
   console.log(evolutionChainResponse);
 }
 
+main().catch((error) => {
+  console.error("An error occurred:", error);
+});
+
 module.exports = {
   fetchEvolutionChain,
   convertToEvolutionChain,
