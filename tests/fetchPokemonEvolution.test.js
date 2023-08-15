1064c603a38227e0c4aac9a60060dacc7946836d
Adam <adam.j.mccor@gmail.com>
2023-08-15 10:49:37 +0100

    one, fetch evolution chain test

--- tests/fetchPokemonEvolution.test.js
+++ tests/fetchPokemonEvolution.test.js
@@ -4,3 +4,18 @@ const {
   getChainForPokemon,
 } = require("../fetchPokemonEvolution");
 const { mockEvolutionChainData } = require("../test/mockPokemonEvolutionData");
+
+describe("fetchEvolutionChain", () => {
+  test("fetchEvolutionChain fetches evolution chain correctly", async () => {
+    const mockPokemonName = "caterpie";
+    const evolutionChain = await fetchEvolutionChain(mockPokemonName);
+    expect(evolutionChain).toEqual(mockEvolutionChainData.chain);
+  });
+  test("fetchEvolutionChain handles errors gracefully", async () => {
+    const mockInvalidPokemonName = "0";
+    expect(fetchEvolutionChain(mockInvalidPokemonName)).rejects.toThrowError(
+      "Pokemon data not found"
+    );
+  });
+});
+
