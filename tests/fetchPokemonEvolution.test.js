5929eccf0cf751afa23ebb9e98ee599fc7ea54a9
Adam <adam.j.mccor@gmail.com>
2023-08-15 01:05:51 +0100

    one, added mock data file

---
+++ tests/fetchPokemonEvolution.test.js
@@ -0,0 +1,6 @@
+const {
+  fetchEvolutionChain,
+  convertToEvolutionChain,
+  getChainForPokemon,
+} = require("../fetchPokemonEvolution");
+const { mockEvolutionChainData } = require("../test/mockPokemonEvolutionData");
